954779f5be4b7c5bca4d6afc54f929ad2659fafb
2017000200-CarlosJuarez <c.juarez.5bdb@gmail.com>
2026-04-23 21:54:40 -0600

    Add files via upload

---
+++ Calculadora_19_4toE_CarlosJuarez.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora_19_4toe_carlosjuarez;
+
+/**
+ *
+ * @author MSI Cougar
+ */
+public class Calculadora_19_4toE_CarlosJuarez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        calcu v1=new calcu();
+        v1.setVisible(true);
+    }
+    
+}
